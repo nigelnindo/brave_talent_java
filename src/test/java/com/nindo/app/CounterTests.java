9daf6f0da96043fc3ba1e1e25c1bfe87cf8d9043
Nigel Nindo <nigelnindo@gmail.com>
2017-08-15 15:13:03 +0300

    Counter tests, refactor Extractor tests

---
+++ src/test/java/com/nindo/app/CounterTests.java
@@ -0,0 +1,52 @@
+package com.nindo.app;
+
+import com.nindo.app.counter.Counter;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by nigelnindo on 8/15/17.
+ */
+public class CounterTests extends TestCase {
+
+    Counter counter = new Counter();
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CounterTests(String testName )
+    {
+        super( testName );
+    }
+
+    public void testApp(){
+        testInvalidInputs();
+        testCount();
+    }
+
+    public void testInvalidInputs(){
+        // make sure that both plates supplies are valid Kenyan plates
+        assertEquals( (Integer) 0, counter.getDifference("KBA 123J","KAY F237"));
+    }
+
+    public void testCount(){
+        /**
+         * -> values that come after one another should have a difference of
+         * -> values different in the tens should still calculate correctly
+         * -> values different in the hundreds should still calculate correctly
+         * -> changing last letter of plate by moving to the next largest figure should have a difference of one
+         * -> moving to new last letter of 'prefix' should result in a difference of one
+         * -> moving to first letter of 'prefix should result in a difference of one
+         */
+
+        assertEquals( (Integer) 0, counter.getDifference("KBA 049A","KBA 050A"));
+        assertEquals( (Integer) 48, counter.getDifference("KBE 050A","KBE 098A"));
+        assertEquals( (Integer) 200, counter.getDifference("KBD 050A","KBD 250A"));
+        assertEquals( (Integer) 1, counter.getDifference("KAA 999A","KAA 001B"));
+        assertEquals( (Integer) 1, counter.getDifference("KAA 999Z","KAB 001A"));
+
+    }
+
+}
--- src/test/java/com/nindo/app/ExtractorTests.java
+++ src/test/java/com/nindo/app/ExtractorTests.java
@@ -1,15 +1,13 @@
 package com.nindo.app;
 
-import com.nindo.app.counter.Counter;
 import com.nindo.app.extractor.Extractor;
-import junit.framework.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Created by nigelnindo on 8/15/17.
  */
-public class ExtractorTest  extends TestCase {
+public class ExtractorTests extends TestCase {
 
     Extractor extractor = new Extractor();
     private static String NEGATIVE_RESULT = "NO PLATE";
@@ -19,17 +17,9 @@ public class ExtractorTest  extends TestCase {
      *
      * @param testName name of the test case
      */
-    public ExtractorTest( String testName )
+    public ExtractorTests(String testName )
     {
         super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ExtractorTest.class );
     }
 
     public void testApp()
@@ -41,12 +31,12 @@ public class ExtractorTest  extends TestCase {
 
     public void testTruePositive(){
         // should get the explicit number plate in the sentence
-        assertEquals( extractor.extract("I saw a KCA 199A on the highway"), "KCA 199A" );
+        assertEquals( "KCA 199A", extractor.extract("I saw a KCA 199A on the highway"));
     }
 
     public void testCaseInsensitive(){
         // should extract the number plate even whether in upper/lower/mixed case
-        assertEquals( extractor.extract("Mercy just bought kBA 951j"), "KBA 951J");
+        assertEquals( "KBA 951J", extractor.extract("Mercy just bought kBA 951j"));
     }
 
     public void testInvalidNumberPlates(){
@@ -55,19 +45,19 @@ public class ExtractorTest  extends TestCase {
          * -> should fail if any of first three characters in plate suffix is a string
          * -> should fail if any of the characters in plate suffix is a number
          */
-        assertEquals( extractor.extract("KBA 9511 was bought by Mercy"), NEGATIVE_RESULT);
-        assertEquals( extractor.extract("KBA 9A5J was bought by Mercy"), NEGATIVE_RESULT);
-        assertEquals( extractor.extract("KB3 951J was bought by Mercy"), NEGATIVE_RESULT);
+        assertEquals( NEGATIVE_RESULT, extractor.extract("KBA 9511 was bought by Mercy"));
+        assertEquals( NEGATIVE_RESULT, extractor.extract("KBA 9A5J was bought by Mercy"));
+        assertEquals( NEGATIVE_RESULT, extractor.extract("KB3 951J was bought by Mercy"));
     }
 
     public void testNoNumberPlateInSentence(){
         // should not return any plate if there isn't a plate in the sentence
-        assertEquals( extractor.extract("She sells sea shells at the sea shore"), NEGATIVE_RESULT);
+        assertEquals( NEGATIVE_RESULT, extractor.extract("She sells sea shells at the sea shore"));
     }
 
     public void testTwoPositivePlatePrefixes(){
         // should ignore 'Ken', which is a positive prefix, but is a name
-        assertEquals( extractor.extract("Yesterday, Ken was driving a KBC 367A"), "KBC 367A" );
+        assertEquals( "KBC 367A", extractor.extract("Yesterday, Ken was driving a KBC 367A"));
     }
 
 
